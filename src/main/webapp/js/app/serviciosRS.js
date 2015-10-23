fd8e727d84dfee07706a34e4729414e92c2b33de
luiscarlosvasquez <luis.vasquez@mail.escuelaing.edu.co>
2015-10-22 22:18:58 -0500

    CorreccionServicesRS

--- src/main/webapp/js/app/serviciosRS.js
+++ src/main/webapp/js/app/serviciosRS.js
@@ -165,7 +165,7 @@
          */
         this.califiEst = function ( idCliente, idEnsayo, calificacione, descripcions ) {    
             var ensayo=$http.get('rest/calificacion/ensayo/'+idEnsayo);
-            var calificacion={'idCalificacion':0,'ensayo':ensayo,'calificacionBanda':0,'calificacionEstablecimiento':calificacione, 'descripcion':descripcions};
+            var calificacion={"idCalificacion":0,"ensayo":ensayo,"calificacionBanda":0,"calificacionEstablecimiento":calificacione, "descripcion":descripcions};
              return $http({
                 method: 'POST',
                 url: 'rest/calificacion/',data:calificacion
